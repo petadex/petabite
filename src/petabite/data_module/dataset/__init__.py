19f2060d27baa71d2934c4e3090a75192141383a
Purav Gupta <guptapurav@gmail.com>
2026-06-17 12:16:37 -0400

    Reorganizing the repo

--- src/petabite/data_module/dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-from petabite.utils import Registry
-
-from .petase_dataset import PETaseDataset
-
-DATASET_REGISTRY = Registry("dataset")
-DATASET_REGISTRY.register("petase")(PETaseDataset)
-
-__all__ = ["DATASET_REGISTRY", "PETaseDataset"]
